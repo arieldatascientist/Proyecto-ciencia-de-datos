bd2d4b47edcfd7172082d14fc47de0d69b16a521
Ariel <ariel.wtf.7@gmail.com>
2025-11-26 16:23:16 -0600

    Analisis de ventas por cliente

---
+++ src/AnalisisEDA_NW.py
@@ -0,0 +1,42 @@
+################# Ánálisis EDA NorthWind ################
+
+import pandas as pd
+import sqlite3 
+import matplotlib.pyplot as plt
+
+conn = sqlite3.connect("NorthWind.db")
+
+#Monto de ventas por cliente
+query1 = ''' 
+        SELECT c.CustomerName AS NombreCliente, sum(od.Quantity * p.Price) AS Monto FROM Customers c
+        JOIN Orders o ON c.CustomerID = o.CustomerID
+        JOIN OrderDetails od ON o.OrderID = od.OrderID
+        JOIN Products P ON od.ProductID = p.ProductID
+        GROUP BY c.CustomerName
+        ORDER BY Monto DESC
+             
+           '''
+
+clientes = pd.read_sql_query(query1, conn) 
+print(clientes.info)
+print(clientes.describe())
+print(clientes.index.duplicated().any())
+print(clientes.isna().sum())
+
+#Podemos destacar que el monto promedio gastado por cliente es $5221.94 USD, con un máximo de $35631.21USD
+#y un mínimo de $62.46 USD.
+
+#Top 10 mejores clientes en ventas gráfica
+clientes_top10 = clientes.head(10)
+
+clientes_top10.plot(x = 'NombreCliente', y = 'Monto', kind = 'bar', figsize=(10, 5))
+plt.title("Top 10 mejores clientes")
+plt.xlabel("Cliente")
+plt.ylabel("Monto")
+plt.xticks(rotation=45)
+plt.grid()
+plt.show()   
+
+#El mejor cliente es Ernst Handel con $35631.21 USD 
+
+
